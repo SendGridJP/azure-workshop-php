370eb116cc71050f6f6c8819884656fa72969a62
xn-- -766a204nded <wataru@kke.co.jp>
2015-01-07 06:15:33 +0000

    first commit

---
+++ Includes/simplecms-config.php
@@ -0,0 +1,9 @@
+<?php
+    define('DB_NAME', getenv('DB_NAME'));
+    define('DB_USER', getenv('DB_USER'));
+    define('DB_PASSWORD', getenv('DB_PASSWORD'));
+    define('DB_HOST', getenv('DB_HOST'));
+
+    define('DEFAULT_ADMIN_USERNAME', 'admin'); 
+    define('DEFAULT_ADMIN_PASSWORD', 'password'); 
+?>
